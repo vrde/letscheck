47f357681b8955bbb30291cd986036036a4a3a3c
lorrandal <lorenzo.alfine@gmail.com>
2020-05-07 14:09:48 +0200

    Optimized news loading and added test for content_index view (#6)
    * Optimized news loading and added test for content_index view
    
    Co-authored-by: Lorenzo <lorenzo@waytation.com>

--- src/content/tests.py
+++ src/content/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class ContentViewTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_index_should_display_news(self):
+        from .views import index
+        from triage.models import News, Rating
+
+        rating = Rating.objects.create(name="rating1")
+
+        news = News.objects.create(
+            title="title1",
+            description="description1",
+            rating=rating)
+
+        response = self.c.get(reverse(index))
+        displayed_news = response.context["news"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(displayed_news), 1)
+        self.assertEqual(displayed_news[0].title, "title1")
+        self.assertEqual(displayed_news[0].description, "description1")
+        self.assertEqual(displayed_news[0].rating.name, "rating1")
